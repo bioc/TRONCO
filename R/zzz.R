997d5d59de0ac04a39aca872d9d68fcb5308774d
Daniele Ramazzotti <daniele.ramazzotti@yahoo.com>
2017-03-13 21:54:14 +0000

    update to 2.7.6
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TRONCO@127359 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,26 +1,15 @@
 #### TRONCO: a tool for TRanslational ONCOlogy
 ####
-#### Copyright (c) 2015-2016, Marco Antoniotti, Giulio Caravagna, Luca De Sano,
+#### Copyright (c) 2015-2017, Marco Antoniotti, Giulio Caravagna, Luca De Sano,
 #### Alex Graudenzi, Giancarlo Mauri, Bud Mishra and Daniele Ramazzotti.
 ####
 #### All rights reserved. This program and the accompanying materials
 #### are made available under the terms of the GNU GPL v3.0
 #### which accompanies this distribution.
 
-# maybe in the future... #
-
-##' @useDynLib TRONCO
-##' @importFrom Rcpp sourceCpp
-#NULL
-#
-
 .onLoad <- function(libname, pkgname) {
 	hypotheses.env <- new.env(parent = emptyenv())
 	assign('hypotheses.env', hypotheses.env, asNamespace(pkgname))
 }
-
-#.onUnload <- function (libpath) {
-#  library.dynam.unload("TRONCO", libpath)
-#}
 
 #### end of file -- as.functions.R
